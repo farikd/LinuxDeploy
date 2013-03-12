9bfcc69eb70d2c5e955c7d6263c8e93591a7bdb7
farikd <farikd@gmail.com>
2013-03-12 13:35:36 +0200

    Create php_ssh2_centos.sh

---
+++ php_ssh2_centos.sh
@@ -0,0 +1,17 @@
+cd /opt/
+wget http://www.libssh2.org/download/libssh2-1.4.3.tar.gz
+tar vzxf libssh2-1.4.3.tar.gz
+cd libssh2-1.4.3
+./configure
+make -j10
+make install
+
+cd /opt/
+wget http://pecl.php.net/get/ssh2
+vxzf ssh2
+cd `ls -rd ssh2-*`
+phpize
+./configure --with-ssh2
+make -j4
+make install
+echo "extension = /usr/lib64/php/modules/ssh2.so" >> /etc/php.ini
