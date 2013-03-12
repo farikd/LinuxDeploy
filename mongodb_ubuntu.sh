47d405624dccb2ffb555fd2982ff993fa41c3eb3
farikd <farikd@gmail.com>
2013-03-12 16:01:53 +0200

    Create mongodb_ubuntu.sh

---
+++ mongodb_ubuntu.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list
+apt-get update
+apt-get install mongodb-10gen
